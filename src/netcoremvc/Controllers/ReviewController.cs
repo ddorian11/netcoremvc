04a4ae17159a359f9c995cdff5fcfdb10b7af2e2
Dorian <Dorian@DESKTOP-A95SUI7>
2016-12-14 23:54:37 +0200

    Add project files.

---
+++ src/netcoremvc/Controllers/ReviewController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using netcoremvc.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace netcoremvc.Controllers
+{
+    public class ReviewController : Controller
+    {
+        
+        public ActionResult Review(UserReview model)
+        {
+            var name = model.Name;
+            var email = model.Email;
+            var comment = model.Comment;
+
+            /*ReviewTable table = new ReviewTable();
+            table.Name = name;
+            table.Email = email;
+            table.Comment = comment;
+            table.DateTime = DateTime.Now;*/
+
+            return View();
+        }
+
+
+    }
+}
---
+++ src/netcoremvc/Models/ReviewModels.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcoremvc.Models
+{
+    public class ReviewTable
+    {
+        [Key]
+        public string Id { get; set; }
+
+        public DateTime DateTime { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        public string Comment { get; set; }
+    }
+}
